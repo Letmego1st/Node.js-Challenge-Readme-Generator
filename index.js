e5f738b5e0b5a38613a39c409200ee8325c97369
letmego1st <imcoding4real@gmail.com>
2023-04-05 15:41:12 -0700

    Next question completed

--- index.js
+++ index.js
@@ -5,7 +5,55 @@ const path = require('path');
 const genMD = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [ {
+    type: 'input',
+    name: 'title',
+    message: 'What is your project title?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'enter brief description of project',
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: 'how to install the project',
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'how to use the project',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub Username',
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: 'how to contribute to this project',
+  },
+  {
+    type: 'input',
+    name: 'test',
+    message: 'how to run the test product',
+  },
+  {
+    type: 'input',
+    name: 'email',
+    message: 'what is your email? ',
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: 'choose a license for your project ',
+    choices: ['MIT','Apache_2.0,"BSD 3',"BSD 2", "GPL 3.0", "none"]
+  o},
+
+];
+
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
